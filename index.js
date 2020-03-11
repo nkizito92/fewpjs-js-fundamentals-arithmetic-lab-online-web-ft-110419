414473cc8a3ac1c36c73799ba60915f2da72d99a
Kizito Njoku <nkizito57@gmail.com>
2020-03-11 15:31:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+// Write your code 
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random(2) * 10)
+
+const mod = 10 % 6
+const max = Math.floor(Math.max(1, 6, 7, 9, 2, 20))
